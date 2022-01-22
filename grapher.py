8c4b0e569006b66a5be1b021f458f6b825c84c68
Manu Cornet <m@ma.nu>
2022-01-22 08:21:06 +0000

    Be a little more robust in finding helper files regardless of current directory

--- grapher.py
+++ grapher.py
@@ -1,13 +1,16 @@
 "Draws visual graphs from statistics"
 
+import os
+import sys
+
 def draw_weeks(weeks, output_file):
     "Draws a weekly stats graph"
     weeks_out = []
     for week in weeks:
         weeks_out.append(week + ":" + ":".join([str(a) for a in weeks[week]]))
 
-    chart_js = open("helpers/Chart.bundle.min.js").read()
-    template = open("template.html").read()
+    chart_js = open(os.path.join(sys.path[0], "helpers/Chart.bundle.min.js")).read()
+    template = open(os.path.join(sys.path[0], "template.html")).read()
     output = template
     output = output.replace("{{CHART_JS}}", chart_js)
     output = output.replace("{{WEEKS}}", "|".join(weeks_out))
